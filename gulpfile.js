d8f69c19625db9f514621d72664980194e02c11d
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-05-03 00:59:55 +0300

    add ticks and labels to timeline scale

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./styles/sass/**/*.scss')
+  return gulp.src('./styles/sass/**/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./styles/css/'));
 });
