e2531d3d2d215ace688cdfa2f26eff5031af73ea
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-05-14 01:13:26 +0300

    recalculate viewport height after each window resize

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-  var viewportHeight = $(window).height();
+  var $window = $(window);
+  var $page = $('.page');
 
-  $('.page').css({
-    height: viewportHeight
-  });
+  var setViewportHeight = function () {
+    var viewportHeight = $window.height();
 
-  $('.page').css({
+    $page.css({
+      height: viewportHeight
+    });
+  };
+
+  setViewportHeight();
+
+  $page.css({
     opacity: 0.0,
     visibility: 'visible'
   }).animate({
     opacity: 1.0
   });
+
+  $window.resize(setViewportHeight);
 });
