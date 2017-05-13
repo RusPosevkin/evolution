11113901774fb6226dce99c30835a0dcf4daddb8
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-05-14 02:08:19 +0300

    make fixed (sticky) part of minimap)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // calculate and set height of viewport
   var $window = $(window);
   var $page = $('.page');
 
@@ -12,6 +13,8 @@ $(document).ready(function() {
 
   setViewportHeight();
 
+  // fading animation during initial render
+  // to hiding change of viewport's height
   $page.css({
     opacity: 0.0,
     visibility: 'visible'
@@ -19,5 +22,33 @@ $(document).ready(function() {
     opacity: 1.0
   });
 
+  // recalculate and set height of viewport
+  // after each window resize
   $window.resize(setViewportHeight);
+
+  // toggle miniMap fixed (sticky)
+  // after 800px (16 columns) horizontal offset scrolling
+  var $miniMap = $('.minimap_main');
+  var isMinimapFixed = false;
+
+  var toggleMinimapFixed = function () {
+    if (window.pageXOffset <= 800) {
+      if (isMinimapFixed)  {
+        $miniMap.removeClass('minimap_fixed');
+        isMinimapFixed = false;
+      }
+    } else {
+      if (!isMinimapFixed) {
+        $miniMap.addClass('minimap_fixed');
+        isMinimapFixed = true;
+      }
+    }
+  };
+
+  // initial calculation
+  // it needs if we scroll and reload the page
+  toggleMinimapFixed();
+
+  // recalculate during scrolling
+  $window.scroll(toggleMinimapFixed);
 });
