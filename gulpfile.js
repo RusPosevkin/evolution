51e1b8a87c378dedf7328d57856c6e670864afa9
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-06-05 22:59:19 +0300

    add CSSO (issue #33)

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var csso = require('gulp-csso');
 
 gulp.task('sass', function () {
   return gulp.src('./styles/sass/**/style.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(csso())
     .pipe(gulp.dest('./styles/css/'));
 });
 
