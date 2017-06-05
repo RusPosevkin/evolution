55abf7aa8107f00f1adfe55999f94a736323da70
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-06-06 00:11:56 +0300

    add css-nano (fix #33)

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var csso = require('gulp-csso');
+var nano = require('gulp-cssnano');
 
 gulp.task('sass', function () {
   return gulp.src('./styles/sass/**/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(csso())
     .pipe(gulp.dest('./styles/css/'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./styles/sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task('optimize', function () {
+    return gulp.src('./styles/sass/**/style.scss')
+        .pipe(sass())
+        .pipe(nano())
+        .pipe(csso())
+        .pipe(gulp.dest('./styles/css'));
 });
